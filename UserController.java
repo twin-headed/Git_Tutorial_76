5796ae8e7aa83877c212be2b57483970bb4c8ace
twin-headed <trionis@naver.com>
2021-03-16 16:04:07 +0900

    Add UserController [Insert]

---
+++ UserController.java
@@ -0,0 +1,15 @@
+@Controller
+public class UserController {
+   // 로그인 페이지로 이동
+   @RequestMapping("/user/login.do)
+   public String login() {
+      logger.info("경로 : login");
+      return "user/login";
+   }
+
+   @RequestMapping("/user/insert.do")
+   public String insert() {
+      logger.info("경로 : insert");
+      return "user/insert";     
+   }
+}  	
